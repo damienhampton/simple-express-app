7905f66692778462c53cd1d7fefb8049794ddfcf
Damien Hampton <damien@26brains.com>
2024-01-28 22:24:57 +0000

    Added redis

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const express = require('express');
+const Redis = require('ioredis');
 const app = express();
 
-app.use((req, res) => {
+const redis = new Redis(6379, "redis-staging")
+
+app.get('/env', (req, res) => {
     res.json({ environment: process.env });
+})
+app.get('/redis', async (req, res) => {
+    res.json({ redis: await redis.info() });
 })
 
 app.listen(process.env.PORT || 3000);
